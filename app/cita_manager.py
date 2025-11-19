c82ea02992384d42a7c23df1d3c481e5a0c92ef0
Astolfo34 <jsebasdroid31@gmail.com>
2025-11-19 14:27:28 -0500

    persistencia agregada , metodos para guardar y cargar jsons en un directorio del SO personal en la ruta home/citas.json (~/citas.json)---> recordar usar logica exclusiva para que un usuario no obtenga todas las citas existentes, sino las que le corresponden

--- app/cita_manager.py
+++ app/cita_manager.py
@@ -1,8 +1,16 @@
+import json
+import os.path
+import os
 from datetime import datetime
 
 class CitaManager:
-    def __init__(self):
-        self.citas = []
+    def __init__(self, file_path="citas.json"):
+        # si no se encuentra el directorio de persistencia, se crea uno en el home del user
+        if file_path is None:
+            file_path = os.path.expanduser("~/citas.json")
+        # se asegura que el directorio exista para la persistencia
+        self.file_path = file_path
+        self.citas = self._load_data()
 
     def agendar_cita(self, paciente, medico, fecha):
         if self._cita_existente(paciente, medico, fecha):
@@ -17,3 +25,13 @@ class CitaManager:
 
     def _cita_existente(self, paciente, medico, fecha):
         return any(cita for cita in self.citas if cita["paciente"] == paciente and cita["fecha"] == fecha)
+
+    def _load_data(self):
+        if not os.path.exists(self.file_path):
+            return []
+        with open (self.file_path, "r") as f:
+            return json.load(f)
+
+    def _save_data(self):
+        with open (self.file_path, "w") as f :
+            json.dump(self.citas,f,indent=4)
